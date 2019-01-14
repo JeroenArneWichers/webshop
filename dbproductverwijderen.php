50026c2b226274d05022cf79f697eab500a80c21
Jeroen Arne Wichers <jwichers@hotmail.com>
2019-01-14 12:26:36 +0100

    working delete

--- dbproductverwijderen.php
+++ dbproductverwijderen.php
@@ -1,20 +1,20 @@
 <?php
 $productid = $_GET['productid'];
 
-try
+try 
 {
-    $conn = new PDO("mysql:host=127.0.0.1;dbname=webshopdb", "root", "");
+    $conn = new PDO("mysql:host=127.0.0.1;dbname=webshopdb", 'root', '');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $conn->prepare("DELETE FROM producten WHERE ID = f:id");
-    
-    $stmt->execute([
-        'fid' => $productid
-]);
-
+    $statement = $conn->prepare('DELETE FROM producten WHERE ID=:fid');
+    $statement->execute([
+    'fid' => $productid
+    ]);
+	
 }
 catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+	
 $conn = NULL;
-?> 
+?>
