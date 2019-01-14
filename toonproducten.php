d5defabe50ee9a030f119f5b1ba2529d393f94b3
Jeroen Arne Wichers <jwichers@hotmail.com>
2019-01-14 14:59:11 +0100

    added search function

--- toonproducten.php
+++ toonproducten.php
@@ -1,8 +1,25 @@
+<form>
+    <input name="naamfilter" placeholder="naam bevat">
+    <input type=submit value="Search">
+</form>
+
+
 <?php
+
+if (isset($_GET['naamfilter']))
+{
+    $naamfilter = $_GET['naamfilter'];
+}
+else
+{
+    $naamfilter = "";
+}
+
 try{
     $conn = new PDO("mysql:host=127.0.0.1;dbname=webshopdb", "root", "");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->query("SELECT * FROM producten");
+
+    $stmt = $conn->query("SELECT * FROM producten WHERE naam LIKE '%$naamfilter%'");
     while ($row = $stmt->fetch()) {
         echo "<li>" . $row['naam'] . " : " . $row['prijs'];
         echo " ";
